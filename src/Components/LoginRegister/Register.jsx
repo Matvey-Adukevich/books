65303da89310718c2628c0e232fdda646263561f
FivelC <elapin2006@gmail.com>
2026-04-16 00:15:02 +0300

    Add state management for buttons

--- src/Components/LoginRegister/Register.jsx
+++ src/Components/LoginRegister/Register.jsx
@@ -1,14 +1,16 @@
 import './LoginRegister.css'
-
+import React, { useState }  from 'react';
 import user_icon from '../Assets/person.png'
 import email_icon from '../Assets/email.png'
 import password_icon from '../Assets/password.png'
 export const Register = () => {
 
+    const [action,setAction] = useState("Sign Up");
+
     return(
         <div className='container'>
             <div className="header">
-                <div className="text">Sign Up</div>
+                <div className="text">{action}</div>
                 <div className="underline"></div>
             </div>
             <div className="inputs">
@@ -31,8 +33,8 @@ export const Register = () => {
             </div>
             <div className="forgot-password">Lost Password? <span>Click Here!</span></div>
             <div className="submit-container">
-                <div className="submit">Sign Up</div>
-                <div className="submit">Login</div>
+                <div className={action=="Login"?"submit gray":"submit"}>Sign Up</div>
+                <div className={action=="Sign Up"?"submit gray":"submit"}>Login</div>
             </div>
         </div>
     )
