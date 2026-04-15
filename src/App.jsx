614404bdecbd245f4e29446ce6285dcddd0ef09f
FivelC <elapin2006@gmail.com>
2026-04-15 23:54:29 +0300

    Add visualization of registration

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+import {Login} from './Components/LoginRegister/Login'
+import {Register} from './Components/LoginRegister/Register'
+function App() {
+  return <Register/>
+}
+
+export default App
